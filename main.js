65e43ca747d54be92336deb4bb59e13d17f593a9
bashirovabduaziz <bashirovabduaziz66@gmail.com>
2026-02-21 18:08:10 +0500

    second

--- main.js
+++ main.js
@@ -1,52 +1,61 @@
-// for (let i = 0; ; i++) {
+// // for (let i = 0; ; i++) {
 
-//     let input = prompt("Iltimos son kiriting");
+// //     let input = prompt("Iltimos son kiriting");
 
-//     if (input === null) {
-//         break;
-//     }
+// //     if (input === null) {
+// //         break;
+// //     }
 
-//     if (input.trim() === "" || isNaN(input)) {
-//         alert("Bu son emas, iltimos son kiriting");
+// //     if (input.trim() === "" || isNaN(input)) {
+// //         alert("Bu son emas, iltimos son kiriting");
+// //         continue;
+// //     }
+
+// //     let num = Number(input);
+
+// //     if (num % 2 === 0) {
+// //         alert("Sizning soningiz juft");
+// //     } else {
+// //         alert("Sizning soningiz toq");
+// //     }
+// // }
+// for (;;) {
+
+//     let son = prompt("Son kiriting");
+//     if (son === null) break;
+
+//     let daraja = prompt("Darajani kiriting");
+//     if (daraja === null) break;
+
+//     if (
+//         son.trim() === "" || 
+//         daraja.trim() === "" || 
+//         isNaN(son) || 
+//         isNaN(daraja) || 
+//         Number(son) === 0 || 
+//         Number(daraja) === 0
+//     ) {
+//         alert("Boshidan kiriting!");
 //         continue;
 //     }
 
-//     let num = Number(input);
+//     son = Number(son);
+//     daraja = Number(daraja);
 
-//     if (num % 2 === 0) {
-//         alert("Sizning soningiz juft");
-//     } else {
-//         alert("Sizning soningiz toq");
+//     let natija = 1;
+
+//     for (let i = 0; i < daraja; i++) {
+//         natija *= son;
 //     }
+
+//     alert("Javob: " + natija);
 // }
-for (;;) {
 
-    let son = prompt("Son kiriting");
-    if (son === null) break;
+let num = +prompt('Son kiriting')
+let sheep = '🐑'
 
-    let daraja = prompt("Darajani kiriting");
-    if (daraja === null) break;
-
-    if (
-        son.trim() === "" || 
-        daraja.trim() === "" || 
-        isNaN(son) || 
-        isNaN(daraja) || 
-        Number(son) === 0 || 
-        Number(daraja) === 0
-    ) {
-        alert("Boshidan kiriting!");
-        continue;
-    }
-
-    son = Number(son);
-    daraja = Number(daraja);
-
-    let natija = 1;
-
-    for (let i = 0; i < daraja; i++) {
-        natija *= son;
-    }
-
-    alert("Javob: " + natija);
+for(let i = 1; i < num; i++){
+    sheep += '🐑'
+    
 }
+    console.log(sheep);
